642994e5152d2c4900c88b2cd64a3fbb8026a6fe
Tyler Neylon <tylerneylon@gmail.com>
2018-09-27 16:38:58 -0700

    lsh1: Minor polish before publishing.

--- lsh_post1/publish.sh
+++ lsh_post1/publish.sh
@@ -2,13 +2,14 @@
 
 ./toboth
 
-# TODO Add temporary link to workshop page.
-
 scp images/*.png images/*.gif neylon@bynomial.com:~/tylerneylon.com/a/lsh1/images
 scp tufte-edited.css lsh_post1.html lsh_post1.pdf lsh_post1_for_kindle.pdf neylon@bynomial.com:~/tylerneylon.com/a/lsh1/
 ssh neylon@bynomial.com "cp ~/tylerneylon.com/a/lsh1/lsh_post1.html ~/tylerneylon.com/a/lsh1/index.html"
 
-# TODO Convert as appropriate for unboxresearch.com and upload there as well.
+# FUTURE Consider converting links on Unbox to stay local to Unbox rather
+#        than pointing over to tylerneylon.com.
+
+patch lsh_post1.html add_unbox_html.patch
 
 cp images/*.png images/*.gif ../../unbox-website/public/articles/images/
 cp tufte-edited.css lsh_post1.html lsh_post1.pdf lsh_post1_for_kindle.pdf ../../unbox-website/public/articles/
