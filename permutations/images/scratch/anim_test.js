deb45c13e39d3b00c74b74225e65420d42cf26ca
Tyler Neylon <tylerneylon@gmail.com>
2021-09-26 12:20:48 -0700

    Add a sample animation.
    This is me getting back into understanding how to make animations in
    javascript.

---
+++ permutations/images/scratch/anim_test.js
@@ -0,0 +1,65 @@
+/* anim_test.js
+ *
+ * Understand how to create a smooth ongoing animation in svg.
+ *
+ * Use:
+ * https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+ *
+ */
+
+
+// ______________________________________________________________________
+// Imports
+
+import * as draw   from './draw.js';
+import * as init   from './init.js';
+
+
+// ______________________________________________________________________
+// Globals
+
+let angle  = 0;
+let circle = null;
+let R      = 0.8;
+let lastTs = null;
+
+let circleStyle = {
+    stroke: 'transparent',
+    fill:   '#888'
+};
+
+
+// ______________________________________________________________________
+// Functions
+
+function drawFrame(ts) {
+
+    let rotationsPerSec = 0.5;
+
+    if (circle === null) {
+        let r = 0.1;
+        circle = draw.circle({x: R, y: 0}, r, circleStyle);
+    } else {
+        angle += rotationsPerSec * 2 * Math.PI * (ts - lastTs) / 1000;
+        let center = {x: R * Math.cos(angle), y: R * Math.sin(angle)};
+        draw.setCenter(circle, center);
+    }
+
+    lastTs = ts;
+    window.requestAnimationFrame(drawFrame);
+}
+
+function animateCircle() {
+    window.requestAnimationFrame(drawFrame);
+}
+
+
+// ______________________________________________________________________
+// Main
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    init.setup();
+
+    animateCircle();
+});
--- permutations/images/scratch/draw.js
+++ permutations/images/scratch/draw.js
@@ -232,6 +232,12 @@ export function circle(center, radius, style, parent) {
     return circle;
 }
 
+export function setCenter(circle, center) {
+    center = mapToCanvasPt(center);
+    circle.setAttribute('cx', center.x);
+    circle.setAttribute('cy', center.y);
+}
+
 export function line(from, to, style, parent) {
 
     if (style === undefined) { style = lightStyle }
