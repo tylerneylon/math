aba524a9afe70f37d39ebe8ef582bc78b408350f
Tyler Neylon <tylerneylon@gmail.com>
2021-12-10 12:33:56 -0800

    draw2: Convert img19!
    This is the last image, and the animation is indeed smoother with
    canvas-based rendering! :)

--- permutations/images/src/img19_g5_in_3d.js
+++ permutations/images/src/img19_g5_in_3d.js
@@ -10,8 +10,8 @@
 
 import * as init   from './init.js';
 import * as matrix from './matrix.js';
-import * as perm   from './perm.js';
-import * as space  from './space.js';
+import * as perm2  from './perm2.js';
+import * as space2 from './space2.js';
 import * as util   from './util.js';
 
 
@@ -31,42 +31,37 @@ let zDist = 7;
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    init.setup();
-
-    let [pts4d, labels] = perm.getGNPointsLessOne(5);
+    let [pts4d, labels] = perm2.getGNPointsLessOne(5);
     // I noticed that #444 looks decent for G_5 and go transparent for G'_5.
-    let [lines, slices] = perm.getEdgeIndexesLex(5, 'transparent');
+    let [lines, slices] = perm2.getEdgeIndexesLex(5, 'transparent');
 
-    // XXX
-    // let [pts, labels]   = perm.getG4PointsIn3D();
-    // let [lines, slices] = perm.getEdgeIndexesLex(4);
-    // let faces           = perm.getG4FacesIn3D();
+    init.addContainerSwitcher((artist) => {
 
-    // Add a small degree of fading for the farther-back points and lines.
-    space.ctx.fadeRange = [0, 11];
+        space2.reset();
+        space2.setArtist(artist);
 
-    // XXX tmp
-    // for (let i in pts) pts[i] = pts[i].slice(0, 3);
+        space2.ctx.fadeRange = [0, 11];
 
-    let pts3d = util.explodeNDPoints(pts4d, labels, 0.7, 2.0);
+        let pts3d = util.explodeNDPoints(pts4d, labels, 0.7, 2.0);
 
-    space.ctx.zoom = 3;
-    space.addPoints(pts3d);
-    space.addLines(lines);
-    // space.addFaces(faces);
+        space2.ctx.zoom = 3;
+        space2.addPoints(pts3d);
+        space2.addLines(lines);
 
-    space.makeDraggable();
+        space2.makeDraggable();
 
-    // Add to the z value of all points.
-    let t = matrix.eye(4);
-    t[2][3] = zDist;
-    space.setTransform(t);
+        // Add to the z value of all points.
+        let t = matrix.eye(4);
+        t[2][3] = zDist;
+        space2.setTransform(t);
 
-    space.ctx.dotSize = 0.035;
-    space.ctx.rotationsPerSec = 0.05;
-    space.ctx.rotationSign = -1;
-    space.setZDist(zDist);
-    space.rotateAround([0.3, -1, 0.5]);
+        space2.ctx.dotSize = 0.035;
+        space2.ctx.rotationsPerSec = 0.05;
+        space2.ctx.rotationSign = -1;
+        space2.setZDist(zDist);
+        space2.rotateAround([0.3, -1, 0.5]);
 
-    space.animate();
+    });
+
+    space2.animate();
 });
