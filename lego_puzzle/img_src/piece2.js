e56f87ec395470bac698d7060719b6dc15ae0018
Tyler Neylon <tylerneylon@gmail.com>
2023-04-17 15:44:53 -0700

    Start to render the full 3x3x3 cube.

---
+++ lego_puzzle/img_src/piece2.js
@@ -0,0 +1,89 @@
+/* piece2.js
+ *
+ */
+
+
+// ______________________________________________________________________
+// Imports
+
+import * as init   from './init.js';
+import * as matrix from './matrix.js';
+import * as space  from './space.js';
+import * as util   from './util.js';
+
+
+// ______________________________________________________________________
+// Globals
+
+let zDist = 8;
+
+let thickStyle = {
+    stroke: '#444',
+    fill:   'transparent',
+    'stroke-width': 3
+};
+
+let thinStyle = {
+    stroke: '#444',
+    fill:   'transparent',
+    'stroke-width': 1
+};
+
+
+// ______________________________________________________________________
+// Main
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    let artist = init.setup();
+    space.setArtist(artist);
+    space.ctx.doDrawDots = false;
+
+    // Set up the piece points and lines.
+    let pts = [];
+    let lines = [];
+
+    let idx = 0;
+    let bottom = [];
+    // These are index stacks.
+    let [xSt, ySt, zSt] = [[], [], []];
+    for (let x = 0; x <= 3; x++) {
+        for (let y = 0; y <= 3; y++) {
+            for (let z = 0; z <= 3; z++) {
+
+                let s = null;
+
+                let pt = [x, y, z];
+                pts.push(pt);
+
+                s = thickStyle;
+                if (x === 0) xSt.push(idx);
+                if (x === 3) lines.push({from: xSt.shift(), to: idx, style: s});
+
+                s = thickStyle;
+                if (y === 0) ySt.push(idx);
+                if (y === 3) lines.push({from: ySt.shift(), to: idx, style: s});
+
+                s = thickStyle;
+                if (z === 0) zSt.push(idx);
+                if (z === 3) lines.push({from: zSt.shift(), to: idx, style: s});
+
+                idx++;
+            }
+        }
+    }
+
+    // Add a small degree of fading for the farther-back points and lines.
+    // space.ctx.fadeRange = [8, 10];
+
+    space.ctx.zoom = 3;
+    space.addPoints(pts);
+    space.addLines(lines);
+
+    space.setTransform(matrix.mult(
+        matrix.translate([-0.3, 2.7, 10]),
+        matrix.rotateAroundX(Math.PI * 0.17),
+        matrix.rotateAroundY(Math.PI * 0.2),
+        matrix.rotateAroundX(Math.PI * 0.5)
+    ));
+});
