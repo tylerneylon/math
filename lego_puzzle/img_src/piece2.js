bdd70ea3a2344383efb33ee6b11316df35b05286
Tyler Neylon <tylerneylon@gmail.com>
2023-04-17 16:11:17 -0700

    Get the edges the way I want them.

--- lego_puzzle/img_src/piece2.js
+++ lego_puzzle/img_src/piece2.js
@@ -29,6 +29,12 @@ let thinStyle = {
     'stroke-width': 1
 };
 
+let blankStyle = {
+    stroke: '#444',
+    fill:   'transparent',
+    'stroke-width': 0
+};
+
 
 // ______________________________________________________________________
 // Main
@@ -56,15 +62,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 let pt = [x, y, z];
                 pts.push(pt);
 
-                s = thickStyle;
+                let [xMod, yMod, zMod] = [x % 3, y % 3, z % 3];
+
+                s = (y === 0 || z === 3) ? thinStyle : blankStyle;
+                if (!yMod && !zMod) s = thickStyle;
                 if (x === 0) xSt.push(idx);
                 if (x === 3) lines.push({from: xSt.shift(), to: idx, style: s});
 
-                s = thickStyle;
+                s = (x === 0 || z === 3) ? thinStyle : blankStyle;
+                if (!xMod && !zMod) s = thickStyle;
                 if (y === 0) ySt.push(idx);
                 if (y === 3) lines.push({from: ySt.shift(), to: idx, style: s});
 
-                s = thickStyle;
+                s = (x === 0 || y === 0) ? thinStyle : blankStyle;
+                if (!xMod && !yMod) s = thickStyle;
                 if (z === 0) zSt.push(idx);
                 if (z === 3) lines.push({from: zSt.shift(), to: idx, style: s});
 
@@ -74,7 +85,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 
     // Add a small degree of fading for the farther-back points and lines.
-    // space.ctx.fadeRange = [8, 10];
+    space.ctx.fadeRange = [10, 17];
 
     space.ctx.zoom = 3;
     space.addPoints(pts);
