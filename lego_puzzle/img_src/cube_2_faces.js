33a9113bf5c5cd65d5aeefc2623d02fc911d9e61
Tyler Neylon <tylerneylon@gmail.com>
2023-05-10 12:35:19 -0700

    Set up two opposing faces of a cube.
    The purpose of this page is to help debug the new (pending) ordering
    algorithm, starting with face-face comparisons (vs, face-edge and
    edge-edge comparisons).

---
+++ lego_puzzle/img_src/cube_2_faces.js
@@ -0,0 +1,48 @@
+/* cube_2_faces.js
+ *
+ * Render a rotating cube with space.js.
+ *
+ */
+
+
+// ______________________________________________________________________
+// Imports
+
+import * as init   from './init.js';
+import * as space  from './space.js';
+import * as util   from './util.js';
+
+
+// ______________________________________________________________________
+// Globals
+
+let zDist = 8;
+
+
+// ______________________________________________________________________
+// Main
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    space.setArtist(init.setup());
+
+    let [pts, lines, faces] = util.getCubePtsLinesFaces();
+    faces[0].style = {fill: '#714'};
+    faces[5].style = {fill: '#16c'};
+
+    // Add a small degree of fading for the farther-back points and lines.
+    space.ctx.fadeRange = [6, 16];
+
+    space.ctx.zoom = 3;
+    space.addPoints(pts);
+    space.addLines(lines);
+    space.addFaces([faces[0], faces[5]]);
+
+    space.makeDraggable();
+    space.ctx.rotationsPerSec = 0.05;
+    space.ctx.rotationSign = -1;
+    space.setZDist(zDist);
+    space.rotateAround([0.3, -1, 0.5]);
+
+    space.animate();
+});
